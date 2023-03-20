2e01f3a2dd24f01cd17cc18f84a954c02f8f8e70
unknown <parkbyeonghak96@gmail.com>
2023-03-20 18:47:27 +0900

    Crate를 사용하여 프론트 파트를 추가했습니다. 페이지 이동 없이 Create를 만들어 작동함.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function Header(props){
   console.log('props', props, props.title);
   return <header>
     <h1><a href="/" onClick={(event)=>{
-      event.preventDefault();
+      event.preventDefault();                                                                     // a태그 작동 안하게 설정
       props.onChangeMode();
     }}>{props.title}</a></h1>
   </header>
@@ -17,7 +17,7 @@ function Nav(props){
     let t = props.topics[i];
     lis.push(<li key={t.id}>
       <a id={t.id} href={'/read/'+t.id} onClick={(event)=>{
-      event.preventDefault();
+      event.preventDefault();                                                                      // a태그 작동 안하게 설정
       props.onChangeMode(Number(event.target.id));
     }}>{t.title}</a>
     </li>);
@@ -34,19 +34,35 @@ function Article(props){
     {props.body}
   </article>
 }
+function Create(props){
+  return <article>
+    <h2>Create</h2>
+    <form onSubmit={event=>{
+      event.preventDefault();                                                                      // a태그 작동 안하게 설정
+      const title = event.target.title.value;
+      const body = event.target.body.value;                                                        // 변수 선언 후 이벤트/타겟/대상/값 의 형식으로 가져옴
+      props.onCreate(title, body);
+    }}>
+      <p><input type="text" name="title" placeholder="title"/></p>
+      <p><textarea name="body" placeholder="body"></textarea></p>
+      <p><input type="submit" value="Create"></input></p>
+    </form>
+  </article>
+}
 function App() {
   const [mode, setMode] = useState('WELCOME');
   const [id, setId] = useState(null);
-  const topics = [
+  const [nextId, setNextId] = useState(4);
+  const [topics, setTopics] = useState([
     {id:1,title:'html', body:'html is ...'},
     {id:2,title:'css', body:'css is ...'},
-    {id:3,title:'javascript', body:'javascript is ...'}
-  ]
-  let content = null;
+    {id:3,title:'javascript', body:'javascript is ...'}                                             // 
+  ]);
+  let content = null;                                                                               // 변수 선언
   if(mode === 'WELCOME'){
     content = <Article title="Welcome" body="Hello, WEB"></Article>
   } else if(mode === 'READ'){
-    let title, body = null;
+    let title, body = null;                                                                         // 변수 선언
     for(let i=0; i<topics.length; i++){
       if(topics[i].id === id){
         title = topics[i].title;
@@ -54,17 +70,31 @@ function App() {
       }
     }
     content = <Article title={title} body={body}></Article>
+  } else if(mode === 'CREATE'){
+    content = <Create onCreate={(_title, _body)=>{
+      const newTopic = {id:nextId, title:_title, body:_body}
+      const newTopics = [...topics]
+      newTopics.push(newTopic);
+      setTopics(newTopics);
+      setMode('READ');
+      setId(nextId);
+      setNextId(nextId+1);
+    }}></Create>
   }
   return (
     <div>
       <Header title="WEB" onChangeMode={()=>{
-        setMode('WELCOME');
+        setMode('WELCOME');                                                                         // WELCOME 모드로 설정
       }}></Header>
       <Nav topics={topics} onChangeMode={(_id)=>{
-        setMode('READ');
+        setMode('READ');                                                                            // READ 모드로 설정
         setId(_id);
       }}></Nav>
       {content}
+      <a href="/create" onClick={event=>{
+        event.preventDefault();                                                                     // a태그 작동 안하게 설정
+        setMode('CREATE');                                                                          // CREATE 모드로 설정
+      }}>Create</a>
     </div>
   );
 }
